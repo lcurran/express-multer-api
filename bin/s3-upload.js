71defb620c6e52ec52c5c899e874f084694142b2
lcurran <lcurran327@gmail.com>
2016-07-08 15:30:36 -0400

    Refactor into upload module
    Thos module is usable from both a command line application and an express api server.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,65 +1,10 @@
 'use strict';
 
-require('dotenv').config();
-//node modules
 const fs = require('fs');
-const crypto = require('crypto');
-//npm packages
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+const uploader = require('../lib/aws-s3-upload');
 
 let filename = process.argv[2] || '';
 
-const mimeType = (data) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream',
-  }, fileType(data));
-};
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
-
-const awsUpload = (file) => {
-  return randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: "public-read",
-      Body: file.data,
-      Bucket: 'lrcurran-bucket-42',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`
-    };
-  })
-  .then((options) => {
-    return new Promise((resolve, reject) => {
-      s3.upload(options, (error, data) => {
-        if (error) {
-          reject(error);
-        }
-
-        resolve(data);
-      });
-    });
-  });
-};
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -73,13 +18,7 @@ const readFile = (filename) => {
   });
 };
 
-
 readFile(filename)
-.then((data) => {
-  let file = mimeType(data);
-  file.data = data;
-  return file;
-})
-.then(awsUpload)
+.then(uploader.awsUpload)
 .then(console.log)
 .catch(console.error);
